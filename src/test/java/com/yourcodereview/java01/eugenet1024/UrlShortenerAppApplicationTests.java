b9981e291d19e60d82ca360b708853e4e50c422f
EugeneT1024 <EugeneT1024@gmail.com>
2021-12-10 11:44:28 +0300

    commit for generated files

---
+++ src/test/java/com/yourcodereview/java01/eugenet1024/UrlShortenerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcodereview.java01.eugenet1024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
